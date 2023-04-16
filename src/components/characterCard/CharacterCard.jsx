4402390af382560621be088102941bb05b536a5f
Ampersander <mr.khan4ik@gmail.com>
2023-04-16 20:07:31 +0600

    modalkuSlomal

--- src/components/characterCard/CharacterCard.jsx
+++ src/components/characterCard/CharacterCard.jsx
@@ -15,42 +15,55 @@ export default  function RootPage() {
 	},[])
 
 	return (
-		<div className="container w-full mx-auto my-5">
+
+		
+		<div className="container w-full mx-auto my-5 relative">
+
+{isOpen && <CharacterModal/>}
+
 			<div className="container flex w-full flex-wrap justify-between  mx-auto">
 	
 
 			{char.map((character)=> (
 				<>
-	
-				<div className="w-5/12 my-5 px-2 py-2 flex rounded-lg  bg-slate-600">
-			 	 <Image src={character.image} key={character.id} alt={character.name} width={250} height={150}/> 
+
+				<div className="w-5/12 my-5 px-2 py-2 flex rounded-lg  bg-slate-600"  key={character.id} 
+					 onClick={(data)=>{
+						setOpen(true)
+					
+						console.log(data)
+						}} >
+		
+			 	 <Image src={character.image} key={character.id} alt={character.name} width={250} height={150} /> 
 					<div className=" flex flex-col justify-between px-4  ">
-						<span className="text-lg font-semibold"  key={character.id} onClick={()=>setOpen(true)}>Name: {character.name}</span>
+					 <span className="text-lg font-semibold "  key={character.id} >
+						Name: {character.name}
+						</span>
 						<div>
 							<span className="text-lg font-semibold">Status: </span>					
-							<span>{character.status}</span>
+							<span key={character.id}>{character.status}</span>
 							<span className={character.status  == 'Alive' ? 'alive' : null}></span>
 							<span className={character.status  == 'Dead' ? 'dead' : null}></span>
 						</div>
 						<div >
 							<span className="text-lg font-semibold">Gender: </span>
-							<span className="">{character.gender}</span>
+							<span className="" key={character.id}> {character.gender}</span>
 						</div>
 						<div>
 							<span className="text-lg font-semibold">Species: </span>
-							<span> {character.species}</span>
+							<span key={character.id}> {character.species}</span>
 						</div>
 						<div>
 							<span className="text-lg font-semibold">Location: </span>
-							<span> {character.location.name}</span>
+							<span key={character.id}> {character.location.name}</span>
 						</div>
 					</div>
+	
 				</div>
-				
+
 				</>
 			))}
 
-{isOpen && <CharacterModal/>}
 
 			</div>
 		</div>
--- src/components/layout/charModal/characterModal.jsx
+++ src/components/layout/charModal/characterModal.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function CharacterModal() {
+export default function CharacterModal(props) {
+ 
+	// const [char,setChar] = useState([])
+
+
 	return (
-		<div className="container w-full absolute blur-lg ">
-			<div>
-				test
+		<div className="container absolute w-full h-screen top-0 left-0 bg-red-700">
+			<div className=" ">
+				{props.name}
 			</div>			
 		</div>
 	)
